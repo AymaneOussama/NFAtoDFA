5b9dd1cf5a1295590c332d8f51b92129f7467691
AymaneOussama <34279694+AymaneOussama@users.noreply.github.com>
2017-12-11 12:25:12 +0000

    Update code.swift

--- code.swift
+++ code.swift
@@ -1,20 +1,41 @@
-// tableaux des noeuds //
-
-let n1 = [ [[2] , [] , [4] , [1]],[[] , [3] , [] , [2,1]],[[2] , [] , [] , [3]],[[] , [] , [3] , [4,3]] ]
-
-
-// // // // // // // // //
-
-for t in n1 {
-    print ("\(t)")
-}
-
-
-
-for i in n1 {
-    for j in i {
-        for k in j {
-            print("\(k)")
-        }
+public class Automate {
+    
+    public let nodes: [String]
+    public let values: [String]
+    public var transTab: [[String]]
+    public let firstState: String
+    public let finalStates: [String]
+    public var inc = 0
+    
+    
+    init (nodes: [String], values: [String], firstState: String, finalStates: [String]) {
+        self.nodes = nodes
+        self.values = values
+        self.firstState = firstState
+        self.finalStates = finalStates
+        self.transTab = []
     }
+    
+    public func test () {
+    //       for i in transTab {
+      //          print("\(i)")
+        //}
+        print("\(transTab[4][0])")
+    }
+    
+    public func addTransition (fstate: String, value: String, lstate: String) {
+        transTab.append([fstate,value,lstate])
+    }
+    
 }
+
+let automate = Automate(nodes: ["q0", "q1", "q2"], values: ["a", "b"], firstState: "q0", finalStates: ["q1","q2"])
+automate.addTransition(fstate:"q0",value:"a",lstate:"q1")
+automate.addTransition(fstate:"q0",value:"a",lstate:"q2")
+automate.addTransition(fstate:"q1",value:"a",lstate:"q1")
+automate.addTransition(fstate:"q1",value:"a",lstate:"q2")
+automate.addTransition(fstate:"q2",value:"b",lstate:"q1")
+automate.addTransition(fstate:"q2",value:"b",lstate:"q3")
+automate.addTransition(fstate:"q3",value:"a",lstate:"q1")
+automate.addTransition(fstate:"q3",value:"a",lstate:"q2")
+automate.test()
