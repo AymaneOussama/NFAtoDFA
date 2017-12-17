bf0a6533cf7ae38f99d6a4fa5cee8c8572ff7618
AymaneOussama <34279694+AymaneOussama@users.noreply.github.com>
2017-12-17 11:07:01 +0100

    Update code.swift

--- code.swift
+++ code.swift
@@ -27,42 +27,80 @@ public class Automate {
     
     public func Nfa2Dfa () {
         var match1: [String] = []
+        var match2: [String] = []
         var Dfa: [(t1:[String], Str:String, t2:[String])] = []
-        var t1r: [String] = []
-        var exists: [[String]] = []
+        var tr1=[String]()
+        tr1.append("a")
         
-        for i in 0...transTab.count-1 {
-            if marked[i]==false {
-                marked[i] = true
-                match1.append(transTab[0][2])
-                for j in 0...transTab.count-1{
-                       if (transTab[i][0]==transTab[j][0] && transTab[i][1]==transTab[j][1] && marked [j] == false) {
-                           match1.append(transTab[j][2])
-                           marked[j] = true
-                       }
-                }
-               
-                t1r.append(transTab[i][0])
-                Dfa += [(t1:t1r, Str:transTab[i][1], t2:match1)]
+
+        for i in 0...transTab.count-1{
+            if (marked[i]==false){
+               marked[i]=true
+               match1.append(transTab[i][2])
+               for j in 0...transTab.count-1{
+                      if (transTab[i][0]==transTab[j][0] && transTab[i][1]==transTab[j][1] && marked [j] == false){
+                       match1.append(transTab[j][2])
+                       marked[j]=true
+                      }
+               }
                 
-                
+               match1=match1.sorted()
+               tr1[0]=transTab[i][0]
+               Dfa += [(t1:tr1, Str:transTab[i][1], t2:match1)]
+               match1=[]
+
             }
         }
-        print(match1)
-        print (Dfa[0].t2)
+        
+        var compteur = Dfa.count-1
+        
+        for i in 0...compteur{
+            if(Dfa[i].t2.count>1){
+                    for j in 0...Dfa.count-1{
+                            if( Dfa[i].t2==Dfa[j].t1){
+                                    break
+                            }
+                            else {
+                                    for trans in self.values{
+                                            for elem in Dfa[i].t2{
+                                                    for z in 0...Dfa.count-1{
+                                                            if (elem == Dfa[z].t1[0] && trans == Dfa[z].Str){
+                                                                    for e in 0...Dfa[z].t2.count-1{
+                                                                        match2.append(Dfa[z].t2[e])
+                                                                    }
+                                                            }
+                                                    }
+                                            }
+                                        Dfa.append((t1:Dfa[i].t2, Str:trans, t2:match2))
+                                        match2=[]
+                                        compteur = Dfa.count-1
+                                        
+                                        
+                                    }
+                                    
+                                        
+                                        
+                            }
+                        break   
+                    }
+            }
+                
+        }
+        for q in 0...Dfa.count-1{
+         print(Dfa[q])   
+        }
+        
+      
     }
-    
 }
 
-let automate = Automate(nodes: ["1", "2", "3", "4"], values: ["a", "b"], firstState: "1", finalStates: ["2"])
-automate.addTransition(fstate:"1",value:"a",lstate:"1")
-automate.addTransition(fstate:"1",value:"a",lstate:"3")
-automate.addTransition(fstate:"1",value:"b",lstate:"2")
-automate.addTransition(fstate:"2",value:"b",lstate:"1")
-automate.addTransition(fstate:"2",value:"b",lstate:"4")
-automate.addTransition(fstate:"3",value:"a",lstate:"4")
-automate.addTransition(fstate:"3",value:"b",lstate:"2")
-automate.addTransition(fstate:"4",value:"a",lstate:"4")
-automate.addTransition(fstate:"4",value:"b",lstate:"2")
+let automate = Automate(nodes: ["0", "1", "2","3"], values: ["a", "b"], firstState: "1", finalStates: ["3"])
+automate.addTransition(fstate:"0",value:"a",lstate:"0")
+automate.addTransition(fstate:"0",value:"b",lstate:"0")
+automate.addTransition(fstate:"0",value:"b",lstate:"1")
+automate.addTransition(fstate:"1",value:"a",lstate:"2")
+automate.addTransition(fstate:"2",value:"b",lstate:"3")
+
+
 automate.test()
 automate.Nfa2Dfa()
